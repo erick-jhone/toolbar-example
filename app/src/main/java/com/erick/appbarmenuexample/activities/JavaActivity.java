ffe5aa293108991030c454b2e4b1e345a9fcd6f3
ERICK JHONE <erick_jhone@outlook.com>
2024-10-30 21:58:09 -0300

    Add questions

--- app/src/main/java/com/erick/appbarmenuexample/activities/JavaActivity.java
+++ app/src/main/java/com/erick/appbarmenuexample/activities/JavaActivity.java
@@ -1,17 +1,15 @@
 package com.erick.appbarmenuexample.activities;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.erick.appbarmenuexample.R;
 
@@ -32,6 +30,47 @@ public class JavaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_java);
 
         initUIComponents();
+        seeQuestions(0);
+        setupListeners();
+    }
+
+    private void setupListeners() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                RadioButton selectedRadioButton = findViewById(selectedId);
+
+                if(selectedRadioButton != null) {
+                    String question = selectedRadioButton.getText().toString();
+                    String answer = "";
+                    if(question.equals(options[0][correctAnswer])){
+                        answer = "Correto";
+                    } else {
+                        answer = "Incorreto";
+                    }
+                    Toast.makeText(JavaActivity.this, answer, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(JavaActivity.this, "Selecione uma opção", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    /**
+     * Atualiza a interface do usuário para exibir uma nova pergunta e suas opções de resposta.
+     *
+     * @param questionIndex O índice da pergunta no array de perguntas que deve ser exibida.
+     */
+    private void seeQuestions(int questionIndex) {
+        String question = questions[questionIndex];
+        textViewQuestion.setText(question);
+        radioGroup.clearCheck();
+        radioButtonFirstOption.setText(options[questionIndex][0]);
+        radioButtonSecondOption.setText(options[questionIndex][1]);
+        radioButtonThirdOption.setText(options[questionIndex][2]);
+        radioButtonFourthOption.setText(options[questionIndex][3]);
     }
 
     private void initUIComponents() {
@@ -42,11 +81,14 @@ public class JavaActivity extends AppCompatActivity {
         radioButtonSecondOption = findViewById(R.id.radioButtonSecondOption);
         radioButtonFourthOption = findViewById(R.id.radioButtonFourthOption);
 
+        button = findViewById(R.id.buttonQuestion);
+        textViewQuestion = findViewById(R.id.textViewQuestion);
+
         questions = getResources().getStringArray(R.array.perguntas);
         options = getOptions();
     }
 
-    private String[] @NonNull [] getOptions() {
+    private String[][] getOptions() {
         return new String[][]{
                 getResources().getStringArray(R.array.opcao0),
                 getResources().getStringArray(R.array.opcao1),
